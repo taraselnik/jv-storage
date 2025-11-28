3e7f1485727d1a4e0ac1f933947d6f7bfc36bd96
Taras Pustovyi <31005050+taraselnik@users.noreply.github.com>
2025-11-28 13:38:59 +0100

    storage/Implement array-based StorageImpl with fixed capacity and key-value replacement

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,28 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.HashMap;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_CAPACITY = 10;
+
+    private final HashMap<K, V> db = new HashMap<>();
+
     @Override
     public void put(K key, V value) {
+        if (db.size() >= MAX_STORAGE_CAPACITY && !db.containsKey(key)) {
+            throw new RuntimeException("Storage is full");
+        }
+        db.put(key, value);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return db.get(key);
     }
 
     @Override
     public int size() {
-        return -1;
+        return db.size();
     }
 }
